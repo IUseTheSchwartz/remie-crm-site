77434805e12fb89ec2bc5a8d7997b4ca08cdb83a
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-08-29 23:21:47 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // local dev server port
-  },
-  build: {
-    outDir: "dist", // Netlify expects build output here
-    sourcemap: false, // optional: disable if not debugging
-  },
+  server: { port: 5173 },
+  build: { outDir: "dist", sourcemap: false },
 });
