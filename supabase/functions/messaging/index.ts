79cb5d2b516f8cf124525457118df5bcd83b096b
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-08-29 21:14:26 -0500

    Delete supabase directory

--- supabase/functions/messaging/index.ts
+++
@@ -1,6 +0,0 @@
-// supabase/functions/messaging/index.ts
-Deno.serve((_req) =>
-  new Response(JSON.stringify({ ok: true, func: "messaging" }), {
-    headers: { "content-type": "application/json" },
-  })
-);
