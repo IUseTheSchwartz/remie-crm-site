ce8f3016939970e3ee38c3dd0b225e5d5b484c74
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-08-29 23:20:17 -0500

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
 // postcss.config.cjs
 module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {}, // TailwindCSS with PostCSS 8
-    autoprefixer: {},           // adds vendor prefixes for browser support
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
   },
 };
