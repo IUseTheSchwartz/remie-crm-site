3bb1006547ea7cc190db618c18ad6f32cbe7bc7a
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-09-01 22:04:02 -0500

    Delete src/pages/MessagesPage.jsx

--- src/pages/MessagesPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function MessagesPage() {
-  return (
-    <div className="flex items-center justify-center min-h-[60vh] text-white/70">
-      <h1 className="text-2xl font-semibold">Messages – Coming Soon…</h1>
-    </div>
-  );
-}
