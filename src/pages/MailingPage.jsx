00b00dcc2dd399359cc5d7886ea5816fc5947c48
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-09-01 22:00:39 -0500

    Delete src/pages/MailingPage.jsx

--- src/pages/MailingPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function MailingPage() {
-  return (
-    <div className="flex items-center justify-center min-h-[60vh] text-white/70">
-      <h1 className="text-2xl font-semibold">Mailing – Coming Soon…</h1>
-    </div>
-  );
-}
