db522a66d705bedae172f82207cb8a0a4407636c
IUseTheSchwartz <83052876+IUseTheSchwartz@users.noreply.github.com>
2025-09-12 11:05:53 -0500

    Update pagesConfig.js

--- src/pagesConfig.js
+++ src/pagesConfig.js
@@ -1,14 +1,10 @@
-// File: src/pagesConfig.js
-
-// Pages that can be toggled on/off
-// If `true`, the page is under maintenance for everyone except jacobprieto@gmail.com
+export const ownerEmail = "jacobprieto@gmail.com";
 
 export const pageToggles = {
   LeadsPage: false,
   PipelinePage: false,
   CalendarPage: false,
-  ReportsPage: true,  // Example: only Jacob can see ReportsPage
+  ReportsPage: false,
   SettingsPage: false,
   ContactsPage: false,
-  AgentShowcase: false,
 };
